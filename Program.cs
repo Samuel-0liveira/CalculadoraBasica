f9348505469310f1a0eca6b9ffcf09f25790b559
Samuel Oliveira <S.SamuelS.a.m@hotmail.com>
2024-11-07 23:21:44 -0300

    Criação do método Soma

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá Mundo!");
+            Console.Clear();
+
+            Soma();
         }
+
+        static void Soma()
+        {
+            Console.Write("Digite o primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            Console.Write("Digite o segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            float resultado = valor1 + valor2;
+
+            Console.WriteLine($"O resultado da soma é: {resultado}"); 
+        } 
     }
 }
