14dc9a34893e68c608f4a7bea9017986133a301d
Samuel Oliveira <S.SamuelS.a.m@hotmail.com>
2024-11-08 22:42:54 -0300

    Criação do método "Divisao"

--- Program.cs
+++ Program.cs
@@ -6,14 +6,13 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Clear();
-
-            Soma();
-            Subtracao();
+            Divisao();
         }
 
         static void Soma()
         {
+            Console.Clear();
+
             Console.Write("Digite o primeiro valor: ");
             float valor1 = float.Parse(Console.ReadLine()!);
             Console.WriteLine("");
@@ -30,6 +29,8 @@ namespace Calculator
 
         static void Subtracao()
         {
+            Console.Clear();
+
             Console.Write("Digite o primeiro valor: ");
             float valor1 = float.Parse(Console.ReadLine()!);
             Console.WriteLine("");
@@ -41,6 +42,24 @@ namespace Calculator
             float resultado = valor1 - valor2;
 
             Console.WriteLine($"O resultado da subtração é: {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.Write("Digite o primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
+
+            Console.Write("Digite o segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
+
+            float resultado = valor1 / valor2;
+
+            Console.WriteLine($"O resultado da divisão é: {resultado}");
             Console.ReadKey();
         }
     }
